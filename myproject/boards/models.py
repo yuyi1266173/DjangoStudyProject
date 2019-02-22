b5409f3d0e8e21c9bc7d7d464845d2f9f8769d16
wuyu <wuyu@xinktech.com>
2019-02-22 15:50:59 +0800

    issue #1 init myproject and board application and board models

---
+++ myproject/boards/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Board(models.Model):
+    """板块"""
+
+    # unique=True : 强制数据库级别字段的唯⼀性
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    """主题"""
+
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    # 外键关联   related_name='topics'表示board.topics可以查看该板块下所有的相关主题
+    # related_name默认为(class_name)_set --> topic_set
+    board = models.ForeignKey(Board, related_name='topics')
+    starter = models.ForeignKey(User, related_name='topics')
+
+
+class Post(models.Model):
+    """帖子/评论"""
+
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts')
+    # related_name='+' 指示Django我们不需要这种反向关系，所以它会被忽略
+    updated_by = models.ForeignKey(User, null=True, related_name='+')
+
+
