717ba580ddf58c3ffc82d9bd7c11fb20b1560f29
wuyu <wuyu@xinktech.com>
2019-02-22 17:56:50 +0800

    issue #2 modify for home page view use template

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,15 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from boards.models import Board
 
 
 def home(request):
     boards = Board.objects.all()
-    board_names = list()
-
-    for board in boards:
-        board_names.append(board.name)
-
-    response_html = '<br>'.join(board_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
