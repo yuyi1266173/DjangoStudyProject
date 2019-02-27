bf98166eda47fc59bc1725873a64e068fcd33f57
wuyu <wuyu@xinktech.com>
2019-02-27 15:31:30 +0800

    issue #3 add test case for home page view

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,3 +1,23 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from boards.views import home
+
+
+class HomeTests(TestCase):
+
+    def test_home_view_status_code(self):
+        """检查请求首页URL后，返回的响应状态码是否正常"""
+
+        url = reverse('home')
+        # print(url)              # /
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        """检查请求首页的URL的时候，是否返回了正确的视图函数"""
+
+        view = resolve('/')
+        self.assertEqual(view.func, home)
+
