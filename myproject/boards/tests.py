2ca81a8b57bb3de94b4ae0f250896c88381a5ed3
wuyu <wuyu@xinktech.com>
2019-02-28 14:57:31 +0800

    issue #5 add boadr_topics in board/view

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.urls import resolve
 
-from boards.views import home
+from boards.views import home, board_topics
+from boards.models import Board
 
 
 class HomeTests(TestCase):
@@ -20,4 +21,24 @@ class HomeTests(TestCase):
 
         view = resolve('/')
         self.assertEqual(view.func, home)
+
+
+class BoardTopicsTests(TestCase):
+
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func, board_topics)
 
