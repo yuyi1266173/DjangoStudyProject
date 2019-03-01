8f0149af382a9abd01b181f80496d26898709c52
wuyu <wuyu@xinktech.com>
2019-03-01 17:35:09 +0800

    issue #9 修改new_topic视图函数, 处理表单提交；修改topics.html文件展示topic信息.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from boards.models import Board
+from boards.models import Board, User, Topic, Post
 
 
 def home(request):
@@ -15,4 +15,15 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()
+        topic = Topic.objects.create(subject=subject, board=board, starter=user)
+        post = Post.objects.create(message=message, topic=topic, created_by=user)
+
+        return redirect('board_topics', pk=board.pk)
+
     return render(request, 'new_topic.html', {'board': board})
