6e21822ee04b1d9a71a734c150d7160eb7acc03c
wuyu <wuyu@xinktech.com>
2019-03-04 11:26:59 +0800

    issue #9 使用Forms.API创建表单

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from boards.models import Board, User, Topic, Post
+from boards.forms import NewTopicForm
 
 
 def home(request):
@@ -15,15 +16,27 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
 
-        user = User.objects.first()
-        topic = Topic.objects.create(subject=subject, board=board, starter=user)
-        post = Post.objects.create(message=message, topic=topic, created_by=user)
+        # 验证form是否有效
+        if form.is_valid():
+            # 将数据保存至数据库 Topic, 并返回Topic的实例对象
+            topic = form.save(commit=False)
+            # 对返回的Topic实例对象修改后重新保存
+            topic.board = board
+            topic.starter = user
+            topic.save()
 
-        return redirect('board_topics', pk=board.pk)
+            Post.objects.create(message=form.cleaned_data.get('message'),
+                                topic=topic,
+                                created_by=user)
+            return redirect('board_topics', pk=board.pk)
+    # 'GET'
+    else:
+        # 初始化一个新的空表单
+        form = NewTopicForm()
 
-    return render(request, 'new_topic.html', {'board': board})
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
