7c200e086c9b3178a80a11ad14b6e68949ee614e
wuyu <wuyu@xinktech.com>
2019-03-05 16:43:13 +0800

    issue #10 登录页面添加Email

--- myproject/accounts/tests.py
+++ myproject/accounts/tests.py
@@ -2,9 +2,9 @@ from django.urls import resolve
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 from accounts.views import signup
+from accounts.forms import SignUpForm
 
 
 class SignUpTests(TestCase):
@@ -25,7 +25,16 @@ class SignUpTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        """
+        The view must contain five inputs: csrf, username, email,password1, password2.
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SuccessfulSignUpTests(TestCase):
@@ -34,6 +43,7 @@ class SuccessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'admin',
+            'email': 'admin@163.com',
             'password1': '1234qwer',
             'password2': '1234qwer',
         }
